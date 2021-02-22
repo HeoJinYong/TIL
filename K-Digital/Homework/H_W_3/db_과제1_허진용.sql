717c9ba64837dae212b01420f01938bb52170c25
HeoJinYong <qwerm792@naver.com>
2021-02-22 15:02:25 +0900

    1

---
+++ K-Digital/Homework/H_W_3/db_과제1_허진용.sql
@@ -0,0 +1,9 @@
+create table Student1
+(no int primary key not null,
+name char(10),
+det char(20),
+addr char(80),
+tel char(20));
+
+insert into student1 values(1,'홍길동','국문학과','서울','010-1111-1111');
+insert into student1 values(2,'고길동','수학과','인천','010-2222-2222');
